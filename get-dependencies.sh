1d2086084f95a00eb30bd59270dd674dc16e1a65
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-09-08 12:33:29 -0400

    install debloated packages

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+
 echo "Installing dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
@@ -28,5 +30,8 @@ pacman -Syu --noconfirm \
 	zlib               \
 	zsync
 
-echo "All done!"
+echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
+chmod +x ./get-debloated-pkgs.sh
+./get-debloated-pkgs.sh --add-opengl libxml2-mini opus-mini
