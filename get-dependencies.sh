8684930566529c59d58aa295d1e6e6eb697f4bca
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-10-24 20:48:25 -0400

    use common packages

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -35,4 +35,4 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-opengl libxml2-mini opus-mini
+./get-debloated-pkgs.sh --add-common
