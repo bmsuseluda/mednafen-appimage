a8bf61f8c85cc3c904d3e557ce0443a67673fd3e
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-08-09 13:47:15 -0400

    update to template (#15)

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,38 +1,45 @@
 #!/bin/sh
 
-set -ex
+set -eu
 
-EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+ARCH=$(uname -m)
 
-echo "Installing dependencies..."
+echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-	base-devel         \
-	cmake              \
-	ccache             \
-	curl               \
-	gcc-libs           \
-	git                \
-	libao              \
-	libdecor           \
-	libpulse           \
-	libx11             \
-	libxrandr          \
-	libxss             \
-	openal             \
-	pipewire-audio     \
-	pkgconf            \
-	python             \
-	pulseaudio         \
-	pulseaudio-alsa    \
-	sdl2               \
-	wget               \
-	xorg-server-xvfb   \
-	zlib               \
-	zsync
+	cmake     \
+	gcc-libs  \
+	libao     \
+	libx11    \
+	libxrandr \
+	libxss    \
+	openal    \
+	pkgconf   \
+	sdl2      \
+	zlib      \
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
-chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-common
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
+
+# Comment this out if you need an AUR package
+#make-aur-package PACKAGENAME
+
+# If the application needs to be manually built that has to be done down here
+SOURCE=$(wget -q https://mednafen.github.io -O - | sed 's/[()",{} ]/\n/g' | grep -oi "https.*files.*xz$" \
+	| head -1 | python -c 'import sys,html;print(html.unescape(sys.stdin.read()), end="")')
+
+wget "$SOURCE" -O ./mednafen.tar.xz
+tar -xf ./mednafen.tar.xz
+
+(
+	cd ./mednafen
+	./configure --prefix="/usr"
+	make -j"$(nproc)"
+	make install
+	make installcheck
+	make clean
+	make distclean
+)
+rm -rf ./mednafen ./mednafen.tar.xz
+
